9c5aa070654a3a2ffd67414353c9209f67f7e12c
David-The-Programmer <davhuzh@gmail.com>
2020-03-26 15:50:17 +0800

    Add starter code for main app

---
+++ devs/david/app.js
@@ -0,0 +1,26 @@
+/**
+ * File: app.js
+ * --------------
+ * File for the main express app
+ */
+
+// Init express app
+// const express = require("express");
+// const app = express();
+
+// Init middlewares (not custom)
+// app.use("...")
+
+// Init custom middlewares
+// const customMiddlewares = require("./middlewares/index.js");
+// customMiddlewares.init(app);
+
+// Init the API controllers
+// const controllers = require("./controllers/index.js");
+// controllers.init(app);
+
+// Set the app to listen on port 8080
+// const PORT = 8080;
+// app.listen(PORT, function () {
+//     console.log(`Back-End Server listening at port ${PORT}`);
+// });
