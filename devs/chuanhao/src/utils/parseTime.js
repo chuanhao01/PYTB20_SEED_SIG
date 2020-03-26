bbc64bf8c37e6dfb1a01b76a0e751ce7f2f244e7
chuanhao01 <chuanhaolim@gmail.com>
2020-03-26 15:57:08 +0800

    Adding utils and timeparser

---
+++ devs/chuanhao/src/utils/parseTime.js
@@ -0,0 +1,21 @@
+/*
+This utils class is mainly for parsing dates in JS for MySQL
+*/
+// Importing libs needed
+const moment = require('moment');
+
+// Creating the main obj to be exported
+const parseTime = {
+    // convert the js time obj into a moment obj and get the mysql timestamp format
+    /**
+     *
+     *
+     * @param {Date} js_date
+     * @returns {string} [is in timestamp format YYYY-MM-DD hh:mm:ss]
+     */
+    convertTimeStamp(js_date){
+        return moment(js_date).format('YYYY-MM-DD hh:mm:ss');
+    }
+};
+
+module.exports = parseTime;
