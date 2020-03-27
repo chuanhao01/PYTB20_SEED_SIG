acecc9b36ea16ee2dc1ddcf52acf571432ee14cb
chuanhao01 <chuanhaolim@gmail.com>
2020-03-27 14:41:08 +0800

    Adding test for things

--- devs/chuanhao/src/test.js
+++ devs/chuanhao/src/test.js
@@ -2,35 +2,35 @@ const db = require('./db/index');
 const utils = require('./utils/index');
 const validator = require('validator');
 
-// var my_user_id;
-// db.users.createNewUser('t0123456i', utils.parseTime.convertTimeStamp(Date.now()), 'bob the builder', '+65 12345678', 'bobthebuilder@yeswecan.com')
-//     .then(
-//         function(user_id){
-//             my_user_id = user_id;
-//             return db.users.getUserDataByUserId(user_id);
-//         }
-//     )
-//     .then(
-//         function(user){
-//             console.log(user);
-//             return user[0].user_id;
-//         }
-//     )
-//     .then(
-//         function(user_id){
-//             return db.users.updateUserInfoByUserId(user_id, 'nani', utils.parseTime.convertTimeStamp(Date.now()), 'dab the bober', '+65 12345678', 'fuckyou@fuck.com'); 
-//         }
-//     )
-//     .then(
-//         function(){
-//             return db.users.getUserDataByUserId(my_user_id);
-//         }
-//     )
-//     .then(
-//         function(user){
-//             console.log(user);
-//         }
-//     );
+var my_user_id;
+db.users.createNewUser('t0123456i', utils.parseTime.convertTimeStamp('1965-1-1'), 'bob the builder', '+65 12345678', 'bobthebuilder@yeswecan.com')
+    .then(
+        function(user_id){
+            my_user_id = user_id;
+            return db.users.getUserDataByUserId(user_id);
+        }
+    )
+    .then(
+        function(user){
+            console.log(user);
+            return user[0].user_id;
+        }
+    )
+    .then(
+        function(user_id){
+            return db.users.updateUserInfoByUserId(user_id, 'nani', utils.parseTime.convertTimeStamp(Date.now()), 'dab the bober', '+65 12345678', 'fuckyou@fuck.com'); 
+        }
+    )
+    .then(
+        function(){
+            return db.users.getUserDataByUserId(my_user_id);
+        }
+    )
+    .then(
+        function(user){
+            console.log(user);
+        }
+    );
 
 // var my_event_id;
 // db.events.createNewEvent('event1 abd', 'this is the test case', utils.parseTime.convertTimeStamp(Date.now()))
@@ -100,5 +100,5 @@ const validator = require('validator');
 //     }
 // );
 
-let date = '2019-02';
-console.log(utils.parseTime.convertTimeStamp(date));
+// let date = '2019-02';
+// console.log(utils.parseTime.convertTimeStamp(date));
