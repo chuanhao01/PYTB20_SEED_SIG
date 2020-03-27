b9ef56db0d7a98dd7c4456a5c5600ccacbc88e8f
David-The-Programmer <davhuzh@gmail.com>
2020-03-27 16:56:57 +0800

    Complete API endpoint for signing up for an event

--- devs/david/controllers/signupController.js
+++ devs/david/controllers/signupController.js
@@ -5,8 +5,10 @@
  */
 
 // Import libraries that are required
+const utils = require("../../chuanhao/src/utils/index.js");
 
 // Import the model needed for CRUD of DB
+const model = require("../../chuanhao/src/db/index.js");
 
 // signup controller object
 const signupController = {
@@ -17,13 +19,58 @@ const signupController = {
         // API endpoint to sign up for an event
         app.post("/api/events/:event_id/signups/", function (req, res) {
             // get all of the required fields to sign up for event
+            const event_id = req.params.event_id;
 
-            // call the db method to add user to database
+            const user_id = req.cookies.token;
+
+            // call the db method to sign user up for event
+            new Promise((resolve) => {
+                resolve(
+                    model.signups.createSignup(event_id, user_id)
+                        .catch(
+                            function (err) {
+                                console.log(err);
+                                res.status(500).send(
+                                    {
+                                        "Error": "Internal Server Error"
+                                    }
+                                );
+                                throw err;
+
+                            }
+                        )
+                )
+            })
+                .then(
+                    function (signup_id) {
+                        res.status(200).send(
+                            {
+                                "signup_id": signup_id
+                            }
+                        );
+                    }
+                )
+                .catch(
+                    function (err) {
+                        console.log(err);
+                    }
+                )
         });
 
         // API endpoint to leave an event
-        app.delete("/api/events/event_id/signups/", function(req, res) {
+        app.delete("/api/events/event_id/signups/", function (req, res) {
             // call the db method to "delete" user from event
+        });
+
+        // API endpoint to view current events user has signed up for
+        app.get("/api/events/u/", function (req, res) {
+            // call the db method to view events of user
+            // model.signups.getEventsUserSignUp()
+        });
+
+        // API endpoint to view current events the user HAS NOT signed up for
+        app.get("/api/events/u/", function (req, res) {
+            // call the db method to view events user HAS NOT signed up for
         });
 
         // API endpoint to take attendance
