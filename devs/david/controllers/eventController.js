1f9d2645189bd97fc7fa8b0b51111160e6f039b9
David-The-Programmer <davhuzh@gmail.com>
2020-03-27 15:52:35 +0800

    Complete API endpoint to get all events

--- devs/david/controllers/eventController.js
+++ devs/david/controllers/eventController.js
@@ -5,8 +5,10 @@
  */
 
 // Import libraries that are required
+const utils = require("../../chuanhao/src/utils/index.js");
 
 // Import the model needed for CRUD of DB
+const model = require("../../chuanhao/src/db/index.js");
 
 // event controller object
 const eventController = {
@@ -22,26 +24,53 @@ const eventController = {
         });
 
         // API endpoint to view all events
-        app.get("/api/events/", function(req, res) {
+        app.get("/api/events/", function (req, res) {
             // call the db method to view all events in database
+            new Promise((resolve) => {
+                resolve(
+                    model.events.getAllEvents()
+                        .catch(
+                            function (err) {
+                                console.log(err);
+                                res.status(500).send(
+                                    {
+                                        "Error": "Internal Server Error"
+                                    }
+                                );
+                                throw err;
+                            }
+                        )
+                )
+            })
+                .then(
+                    function (allEventData) {
+                        res.status(200).send(allEventData);
+                    }
+                )
+                .catch(
+                    function(err) {
+                        console.log(err);
+                    }
+                )
+
         });
-        
+
         // API endpoint to view events by id
-        app.get("/api/events/:event_id/", function(req, res) {
+        app.get("/api/events/:event_id/", function (req, res) {
             const event_id = req.params.event_id;
             // call the db method to view events by id
         });
 
         // API endpoint to view current events user has signed up for
-        app.get("/api/events/u/", function(req, res) {
+        app.get("/api/events/u/", function (req, res) {
             // call the db method to view events of user
         });
 
         // API endpoint to view current events the user HAS NOT signed up for
-        app.get("/api/events/u/", function(req, res) {
+        app.get("/api/events/u/", function (req, res) {
             // call the db method to view events user HAS NOT signed up for
         });
-        
+
 
     }
 }
