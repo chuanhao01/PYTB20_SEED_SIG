b329c5ad329f68c1c1ed12ae10d523f4506d6602
David-The-Programmer <davhuzh@gmail.com>
2020-03-27 12:09:20 +0800

    Add middleware to app.js

--- devs/david/app.js
+++ devs/david/app.js
@@ -5,22 +5,30 @@
  */
 
 // Init express app
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
 // Init middlewares (not custom)
-// app.use("...")
+// Setting up body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Setting up cookie parser
+const cookieParser = require('cookie-parser');
+const COOKIE_SECRET = process.env.COOKIE_SECRET;
+app.use(cookieParser(COOKIE_SECRET));
 
 // Init custom middlewares
 // const customMiddlewares = require("./middlewares/index.js");
 // customMiddlewares.init(app);
 
 // Init the API controllers
-// const controllers = require("./controllers/index.js");
-// controllers.init(app);
+const controllers = require("./controllers/index.js");
+controllers.init(app);
 
 // Set the app to listen on port 8080
-// const PORT = 8080;
-// app.listen(PORT, function () {
-//     console.log(`Back-End Server listening at port ${PORT}`);
-// });
+const PORT = 8080;
+app.listen(PORT, function () {
+    console.log(`Back-End Server listening at port ${PORT}`);
+});
