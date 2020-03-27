256b376fc962b39360ae4e62a5a369c54374cc93
chuanhao01 <chuanhaolim@gmail.com>
2020-03-27 12:02:45 +0800

    Added custom models for events based on user signup
    get events the user signed up for
    get events the user has not signed up for
    get events the user has participated in

--- devs/chuanhao/src/db/signupsdb.js
+++ devs/chuanhao/src/db/signupsdb.js
@@ -85,6 +85,78 @@ const signupsdb = {
                 }
             });
         });
+    },
+
+    // Specific API calls
+    /**
+     * Get the events the user has not signup for
+     *
+     * @param {string} user_id
+     * @returns {Promise} [arr of evtns]
+     */
+    getEventsUserHasNotSignUp(user_id){
+        return new Promise((resolve, reject) => {
+            this.pool.query(`
+            SELECT e.*
+            FROM EVENTS e
+            LEFT JOIN (SELECT * FROM SIGNUPS
+            WHERE ((user_id = ?) AND (deleted = 0))) sg ON e.event_id = sg.event_id
+            WHERE sg.event_id iS NULL;
+            `, [user_id], function(err, data){
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(data);
+                }
+            });
+        });
+    },
+    /**
+     * Gets the events the user has signed up for
+     * For this, we are getting status = 0
+     * @param {String} user_id
+     * @returns {Promise} [arr of events]
+     */
+    getEventsUserSignUp(user_id){
+        return new Promise((resolve, reject) => {
+            this.pool.query(`
+            SELECT e.*
+            FROM SIGNUPS sg
+            LEFT JOIN EVENTS e ON sg.event_id = e.event_id
+            WHERE ((sg.user_id = ?) AND (sg.deleted = 0) AND (sg.status = 0))
+            `, [user_id], function(err, data){
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(data);
+                }
+            });
+        });
+    },
+    /**
+     * Gets the events the user has already participated in
+     * For this, in the SIGNUPS table, status = 1
+     * @param {String} user_id
+     * @returns {Promise} [arr of events]
+     */
+    getEventsUserParticipated(user_id){
+        return new Promise((resolve, reject) => {
+            this.pool.query(`
+            SELECT e.*
+            FROM SIGNUPS sg
+            LEFT JOIN EVENTS e ON sg.event_id = e.event_id
+            WHERE ((sg.user_id = ?) AND (sg.deleted = 0) AND (sg.status = 1))
+            `, [user_id], function(err, data){
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(data);
+                }
+            });
+        });
     }
 };
 
