5fa9c5274d95627221f248857a2a5779253d599c
chuanhao01 <chuanhaolim@gmail.com>
2020-04-01 16:57:11 +0800

    Grab correct utils this time

--- devs/chuanhao/src/utils/index.js
+++ devs/chuanhao/src/utils/index.js
@@ -1,30 +1,13 @@
-// Main entry point for getting access to the database
+// Importing the other utils libs
+const parseTime = require('./parseTime');
+const jwtToken = require('./jwtToken');
+const email = require('./email');
 
-// Importing mysql lib and setting up the pool obj
-const mysql = require('mysql');
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'localhost',
-    port: 3306,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: 'PYTB_SEED_SIG',
-    insecureAuth: true,
-});
-
-// Importing the other db files here
-const usersdb = require('./usersdb');
-const eventsdb = require('./eventsdb');
-const signupsdb = require('./signupsdb');
-usersdb.init(pool);
-eventsdb.init(pool);
-signupsdb.init(pool);
-
-// Main object being exported, you will mainly be accessing the db functions through this object
-const dataAccess = {
-    users: usersdb,
-    events: eventsdb,
-    signups: signupsdb,
+// Creating the main obj to export
+const utils = {
+    parseTime,
+    jwtToken,
+    email,
 };
 
-module.exports = dataAccess;
+module.exports = utils;
