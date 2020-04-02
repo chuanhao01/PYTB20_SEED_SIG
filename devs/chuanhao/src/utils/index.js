eaab8917146e1e727dd63b5de1d96371666ec25b
chuanhao01 <chuanhaolim@gmail.com>
2020-04-02 15:50:06 +0800

    Utils update needed for my testing

--- devs/chuanhao/src/utils/index.js
+++ devs/chuanhao/src/utils/index.js
@@ -1,13 +1,13 @@
 // Importing the other utils libs
 const parseTime = require('./parseTime');
-const jwtToken = require('./jwtToken');
-const email = require('./email');
+// const jwtToken = require('./jwtToken');
+// const email = require('./email');
 
 // Creating the main obj to export
 const utils = {
     parseTime,
-    jwtToken,
-    email,
+    // jwtToken,
+    // email,
 };
 
 module.exports = utils;
