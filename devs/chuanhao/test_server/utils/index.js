acac2cbd99cb7a220b6427c0a8dcb1fea46728b6
chuanhao01 <chuanhaolim@gmail.com>
2020-04-03 17:17:54 +0800

    Adding the utils and new maths for js ms to s

--- devs/chuanhao/test_server/utils/index.js
+++ devs/chuanhao/test_server/utils/index.js
@@ -1,10 +1,12 @@
 // Importing the other utils libs
+const maths = require('./maths');
 const parseTime = require('./parseTime');
 const jwtToken = require('./jwtToken');
 const email = require('./email');
 
 // Creating the main obj to export
 const utils = {
+    maths,
     parseTime,
     jwtToken,
     email,
