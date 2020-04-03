3aeecc023048955a83ef911d92ae41ea6a457916
chuanhao01 <chuanhaolim@gmail.com>
2020-04-03 17:16:07 +0800

    Adding in the new oauth2 token method model

---
+++ devs/chuanhao/src/db/accountsdb.js
@@ -0,0 +1,81 @@
+/**
+ * Main db file for anything to do with api keys and refresh tokens
+ */
+
+//  Getting the utils in the model
+const utils = require('../../test_server/utils');
+
+// Main obj getting exported
+const accountsdb = {
+    // Setting up the pool property in the object
+    /**
+     *
+     *
+     * @param {mysql_obj_pool} pool
+     */
+    init(pool){
+        this.pool = pool;
+    },
+
+    // For the refresh tokens
+    /**
+     * Creates the refresh token for the user
+     * Should be used when a user is registered
+     *
+     * @param {String} user_id
+     * @returns {Promise} [mysql data]
+     */
+    createRefreshToken(user_id){
+        return new Promise((resolve, reject) => {
+            utils.jwtToken.createRefreshToken()
+            .then(
+                function(refresh_token){
+                    this.pool.query(`
+                    INSERT INTO REFRESH_TOKENS
+                    (user_id, refresh_token, deleted)
+                    VALUES
+                    (?, ?, ?)
+                    `, [user_id, refresh_token, 0], function(err, data){
+                        if(err){
+                            reject(err);
+                        }
+                        else{
+                            resolve(refresh_token);
+                        }
+                    });
+                }.bind(this)
+            )
+            .catch(
+                function(err){
+                    // Just for the jwt errors to be passed up
+                    reject(err);
+                }
+            );
+        });
+    },
+    /**
+     * Model to get the user tagged to the refresh token
+     * No user is returned, reject the refresh token
+     * Done on controller side
+     *
+     * @param {String} refresh_token
+     * @returns {Promise} [arr of user_id] (Controller needs to check)
+     */
+    getUserByRefreshToken(refresh_token){
+        return new Promise((resolve, reject) => {
+            this.pool.query(`
+            SELECT * FROM REFRESH_TOKENS
+            WHERE ((refresh_token = ?) AND (deleted = 0)) 
+            `, [refresh_token], function(err, data){
+                if(err){
+                    reject(err);
+                }
+                else{
+                    resolve(data);
+                }
+            });
+        });
+    }
+};
+
+module.exports = accountsdb;
