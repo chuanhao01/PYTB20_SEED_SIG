19a1c52399070b273c077538f4c8acadc4ad10ec
chuanhao01 <chuanhaolim@gmail.com>
2020-04-05 22:55:18 +0800

    Some added test here and there

--- devs/chuanhao/test_jwt/index.js
+++ devs/chuanhao/test_jwt/index.js
@@ -2,15 +2,16 @@ const jwt = require('jsonwebtoken');
 const key = 'CAts';
 
 const payload = {
-    'data': 'hello'
+    'iat' : Math.floor((Date.now()) / 1000),
 };
 
 const test_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODU3MjM5OTMsImRhdGEiOiJoZWxsbyIsImlhdCI6MTU4NTcyMzk4M30.a10wK9_jVMRVhzUHr7gSN5OkwUtj19Om3d89agY6ym4';
 
 jwt.sign(payload, key, function(err, token){
-    jwt.verify(token, key, function(err, decoded){
-        console.log(decoded);
-    });
+    console.log(token);
+    // jwt.verify(token, key, function(err, decoded){
+        // console.log(decoded);
+    // });
 });
 
 // jwt.verify(test_token, key, function(err, decoded){
