6f9acf245d7bf8b0b06a864d3f88326f5c492d55
David-The-Programmer <davhuzh@gmail.com>
2020-04-07 17:15:16 +0800

    Complete validation for update user by id (user)

--- devs/david/controllers/userController.js
+++ devs/david/controllers/userController.js
@@ -569,56 +569,133 @@ const userController = {
         });
 
         // API endpoint to update user by id
-        app.put("/api/users/u", function (req, res) {
-            // user id
-            const user_id = req.user.user_id;
+        app.put("/api/users/u",
+            // always sanitise inputs to remove all tags if present (XSS Prevention)
+            [
+                body("nric")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .custom(value => {
+                        return /^[sftg]\d{7}[a-z]|^[SFTG]\d{7}[A-Z]/.test(value);
+                    })
+                    .withMessage("NRIC is not in correct format"),
+                body("dob")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .isISO8601()
+                    .withMessage("Date is not in YYYY-MM-DD ISO8601 format"),
+                body("fullname")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .trim().not().isEmpty()
+                    .withMessage("Name is empty"),
+                body("contact_num")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .isMobilePhone("en-SG", { strictMode: true })
+                    .withMessage("Mobile number is not of correct SG format"),
+                body("email")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .isEmail()
+                    .withMessage("Email is not in correct format"),
+                body("PDPA")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .isInt({
+                        min: 0,
+                        max: 1
+                    })
+                    .withMessage("PDPA is not a integer of either 0 or 1"),
+            ],
+            function (req, res) {
+                // do the validation
+                const validationErrors = validationResult(req);
+                // if validation contains any errors, 
+                // throw error to stop it from doing model calls
+                if (!validationErrors.isEmpty()) {
+                    console.log(validationErrors);
+                    res.status(422).send(
+                        {
+                            "Error": "Unprocessable Entity"
+                        }
+                    );
+                    throw validationErrors;
+                }
+                // if validation / sanitization has no errors, start promise chain
+                // user id
+                const user_id = req.user.user_id;
 
-            // nric of user
-            const nric = req.body.nric.toLowerCase();
+                // nric of user
+                const nric = req.body.nric.toLowerCase();
 
-            // date of birth of user (date format)
-            const dob = utils.parseTime.convertTimeStamp(req.body.dob);
+                // date of birth of user (date format)
+                const dob = utils.parseTime.convertTimeStamp(req.body.dob);
 
-            // fullname of user
-            const fullname = req.body.fullname.toLowerCase();
+                // fullname of user
+                const fullname = req.body.fullname.toLowerCase();
 
-            // contact number of user
-            const contact_num = req.body.contact_num;
+                // contact number of user
+                const contact_num = req.body.contact_num;
 
-            // email of user
-            const email = req.body.email.toLowerCase();
+                // email of user
+                const email = req.body.email.toLowerCase();
 
-            // PDPA
-            const PDPA = parseInt(req.body.PDPA);
+                // PDPA
+                const PDPA = parseInt(req.body.PDPA);
 
-            return new Promise((resolve) => {
-                resolve(
-                    model.users.updateUserInfoByUserId(user_id, nric, dob, fullname, contact_num, email, PDPA)
-                        .catch(
-                            function (err) {
-                                console.log(err);
-                                res.status(500).send(
-                                    {
-                                        "Error": "Internal Server Error"
-                                    }
-                                );
-                                throw err;
-                            }
-                        )
+                return new Promise((resolve) => {
+                    resolve(
+                        model.users.updateUserInfoByUserId(user_id, nric, dob, fullname, contact_num, email, PDPA)
+                            .catch(
+                                function (err) {
+                                    console.log(err);
+                                    res.status(500).send(
+                                        {
+                                            "Error": "Internal Server Error"
+                                        }
+                                    );
+                                    throw err;
+                                }
+                            )
 
-                )
-            })
-                .then(
-                    function () {
-                        res.status(204).send();
-                    }
-                )
-                .catch(
-                    function (err) {
-                        console.log(err);
-                    }
-                )
-        });
+                    )
+                })
+                    .then(
+                        function () {
+                            res.status(204).send();
+                        }
+                    )
+                    .catch(
+                        function (err) {
+                            console.log(err);
+                        }
+                    )
+            });
 
         // API endpoint to update user by id (ADMIN)
         app.put("/api/users/:user_id", function (req, res) {
