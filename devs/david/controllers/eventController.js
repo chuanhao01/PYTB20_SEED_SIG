eb83fc06f4a3fd068d4838fcb4564a22fac82829
David-The-Programmer <davhuzh@gmail.com>
2020-04-07 21:18:28 +0800

    Complete validation for update event by id(admin)

--- devs/david/controllers/eventController.js
+++ devs/david/controllers/eventController.js
@@ -273,93 +273,158 @@ const eventController = {
         });
 
         // API endpoint to to update an event
-        app.put("/api/events/:event_id", function (req, res) {
-            // event id
-            const event_id = req.params.event_id;
+        app.put("/api/events/:event_id",
+            [
+                param("event_id")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .isUUID()
+                    .withMessage("Invalid UUID"),
+                body("title")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .trim().not().isEmpty()
+                    .withMessage("Title is empty"),
+                body("description")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .trim().not().isEmpty()
+                    .withMessage("Description is empty"),
+                body("event_date")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .isISO8601()
+                    .withMessage("Date is not in YYYY-MM-DD ISO8601 format"),
+                body("status")
+                    .customSanitizer(value => {
+                        return sanitizeHtml(value, {
+                            allowedTags: [],
+                            allowedAttributes: {}
+                        });
+                    })
+                    .isInt({
+                        min: 0,
+                        max: 1
+                    })
+                    .withMessage("Status is not a integer of either 0 or 1"),
+            ],
+            function (req, res) {
+                // do the validation
+                const validationErrors = validationResult(req);
+                // if validation contains any errors, 
+                // throw error to stop it from doing model calls
+                if (!validationErrors.isEmpty()) {
+                    console.log(validationErrors);
+                    res.status(422).send(
+                        {
+                            "Error": "Unprocessable Entity"
+                        }
+                    );
+                    throw validationErrors;
+                }
+                // if validation / sanitization has no errors, start promise chain
+                // event id
+                const event_id = req.params.event_id;
 
-            // title of event
-            const title = req.body.title.toLowerCase();
+                // title of event
+                const title = req.body.title.toLowerCase();
 
-            // description of event
-            const description = req.body.description.toLowerCase();
+                // description of event
+                const description = req.body.description.toLowerCase();
 
-            // date of event (date format)
-            const event_date = utils.parseTime.convertTimeStamp(req.body.event_date);
+                // date of event (date format)
+                const event_date = utils.parseTime.convertTimeStamp(req.body.event_date);
 
-            // status of the event
-            const status = parseInt(req.body.status);
+                // status of the event
+                const status = parseInt(req.body.status);
 
-            // check if the event exists first, 
-            // then update event
-            return new Promise((resolve) => {
-                resolve(
-                    model.events.checkIfEventExist(event_id)
-                        .catch(
-                            function (err) {
-                                console.log(err);
-                                res.status(500).send(
-                                    {
-                                        "Error": "Internal Server Error"
+                // check if the event exists first, 
+                // then update event
+                return new Promise((resolve) => {
+                    resolve(
+                        model.events.checkIfEventExist(event_id)
+                            .catch(
+                                function (err) {
+                                    console.log(err);
+                                    res.status(500).send(
+                                        {
+                                            "Error": "Internal Server Error"
+                                        }
+                                    );
+                                    throw err;
+                                }
+                            )
+                    )
+                })
+                    // if event exists, resolve
+                    // if not, reject
+                    .then(
+                        function (eventExists) {
+                            return new Promise((resolve, reject) => {
+                                if (eventExists) {
+                                    resolve(true);
+                                } else {
+                                    reject("Event does not exists");
+                                }
+                            })
+                                .catch(
+                                    function (err) {
+                                        console.log(err);
+                                        res.status(500).send(
+                                            {
+                                                "Error": "Internal Server Error"
+                                            }
+                                        );
+                                        throw err;
                                     }
-                                );
-                                throw err;
-                            }
-                        )
-                )
-            })
-                // if event exists, resolve
-                // if not, reject
-                .then(
-                    function (eventExists) {
-                        return new Promise((resolve, reject) => {
-                            if (eventExists) {
-                                resolve(true);
-                            } else {
-                                reject("Event does not exists");
-                            }
-                        })
-                            .catch(
-                                function (err) {
-                                    console.log(err);
-                                    res.status(500).send(
-                                        {
-                                            "Error": "Internal Server Error"
-                                        }
-                                    );
-                                    throw err;
-                                }
-                            )
-                    }
-                )
-                .then(
-                    function () {
-                        return model.events.updateEventDataByEventId(event_id, title, description, event_date, status)
-                            .catch(
-                                function (err) {
-                                    console.log(err);
-                                    res.status(500).send(
-                                        {
-                                            "Error": "Internal Server Error"
-                                        }
-                                    );
-                                    throw err;
-                                }
-                            )
+                                )
+                        }
+                    )
+                    .then(
+                        function () {
+                            return model.events.updateEventDataByEventId(event_id, title, description, event_date, status)
+                                .catch(
+                                    function (err) {
+                                        console.log(err);
+                                        res.status(500).send(
+                                            {
+                                                "Error": "Internal Server Error"
+                                            }
+                                        );
+                                        throw err;
+                                    }
+                                )
 
-                    }
-                )
-                .then(
-                    function () {
-                        res.status(204).send();
-                    }
-                )
-                .catch(
-                    function (err) {
-                        console.log(err);
-                    }
-                )
+                        }
+                    )
+                    .then(
+                        function () {
+                            res.status(204).send();
+                        }
+                    )
+                    .catch(
+                        function (err) {
+                            console.log(err);
+                        }
+                    )
 
-        });
+            });
 
         // API endpoint to view current events user has signed up for
         app.get("/api/events/signed_up/u", function (req, res) {
